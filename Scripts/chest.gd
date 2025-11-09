dfeca4515cb4c3bbae4339f2b7de1fac51f41089
Owen-Harper25 <pistongames25@gmail.com>
2025-11-08 17:39:52 -0800

    Chest dynamic hitbox

--- Scripts/chest.gd
+++ Scripts/chest.gd
@@ -8,14 +8,20 @@ extends StaticBody2D
 @export var coin_spread_angle: float = PI / 2
 @export var coin_min_speed: float = 20.0
 @export var coin_max_speed: float = 70.0
+@onready var collision_polygon_2d_2: CollisionPolygon2D = $CollisionPolygon2D2
+@onready var collision_polygon_2d: CollisionPolygon2D = $CollisionPolygon2D
 
 func _ready() -> void:
 	Interactable.interact = _on_interact
+	collision_polygon_2d.disabled = true
+	collision_polygon_2d_2.disabled = false
 			
 func _on_interact():
 	if Interactable.player and sprite_2d.frame == 0:
 		Interactable.is_interactable = false
 		sprite_2d.frame = 1
+		collision_polygon_2d.disabled = false
+		collision_polygon_2d_2.disabled = true
 		animation_player.play("open_chest")
 		SoundLibrary.play_random_pickup()
 		print("Chest Opened")
